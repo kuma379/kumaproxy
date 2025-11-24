967bb3f3f10958be8de7b18c737a95db07bce3a9
kuma379 <145721266+kuma379@users.noreply.github.com>
2025-11-24 14:07:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ if [ -f "proxy" ]; then
     sleep 1
     echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
 fi
-wget -q https://github.com/JoakimTheCoder/AJTermux/raw/main/proxy
+wget -q https://github.com/kuma379/kumaproxy/raw/main/proxy
 sleep 1
-echo -e "${GREEN}AJProxy is now Installed.${ENDCOLOR}"
+echo -e "${GREEN}KPProxy is now Installed.${ENDCOLOR}"
 echo -e "${GREEN}Execute proxy with this command: ./proxy${ENDCOLOR}"
 chmod +x proxy
